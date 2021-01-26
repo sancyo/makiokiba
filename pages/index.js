de2574c490f78d300503dc61d173170bf3106a43
sancyo <b1845958@planet.kanazawa-it.ac.jp>
2021-01-26 16:06:34 +0900

    add:scssを導入

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import "../styles/variables.scss";
 
 export default function Home() {
   return (
@@ -46,6 +47,7 @@ export default function Home() {
             </p>
           </a>
         </div>
+        <div className="example">Hello World!</div>
       </main>
 
       <footer>
