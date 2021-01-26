d8cfa9f1ea480adaf0db29b6ca098dca73de85db
sancyo <b1845958@planet.kanazawa-it.ac.jp>
2021-01-26 23:45:29 +0900

    add:制作物ページを追加

---
+++ components/molecules/OutputCard.js
@@ -0,0 +1,33 @@
+import "../../styles/components/OutputCard.scss";
+
+const OutputCard = (props) => {
+  console.log(props.value);
+  const item = props.value;
+  return (
+    <div className="output-card">
+      <time className="output-date">{item.date}</time>
+      <h3 className="output-title">{item.title}</h3>
+      <span className="output-type">{item.type}</span>
+      <p className="output-desc">{item.desc}</p>
+      <div className="tag-list">
+        {item.devTag.map((i, id) => {
+          return (
+            <span className="dev-tag" value={i} key={id}>
+              #{i.tag}
+            </span>
+          );
+        })}
+      </div>
+      <a
+        className="output-link"
+        href={item.link}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {item.link}
+      </a>
+    </div>
+  );
+};
+
+export default OutputCard;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,46 @@
 import Head from "next/head";
+import OutputCard from "../components/molecules/OutputCard";
 import "../styles/global.scss";
+import "../styles/pages/index.scss";
 
-export default function Home() {
+export default function Home({ output }) {
+  const outputArray = output.slice();
+  console.log(outputArray);
+  console.log("hoge");
   return (
     <div className="container">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <main className="container main-container">
+        <div className="inner main-inner">
+          <h2 className="main-title">制作物</h2>
+          <div className="output-card-list">
+            {outputArray.map((item) => {
+              return <OutputCard value={item} key={item.id} />;
+            })}
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const key = {
+    headers: { "X-API-KEY": process.env.API_KEY },
+  };
+  const data = await fetch(
+    "https://designdock02.microcms.io/api/v1/output",
+    key
+  )
+    .then((res) => res.json())
+    .catch(() => null);
+  return {
+    props: {
+      output: data.contents,
+    },
+  };
+};
