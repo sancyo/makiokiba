a4c224d5521ed2b0d0b870c47c6a24f9d6b18f5e
sancyo <b1845958@planet.kanazawa-it.ac.jp>
2021-01-26 19:53:45 +0900

    add:各ページを追加

--- pages/career.js
+++ pages/career.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
-import "../styles/global.scss";
 
-export default function Home() {
+const Career = () => {
   return (
     <div className="container">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h2>career page</h2>
     </div>
   );
-}
+};
+export default Career;
--- pages/skill.js
+++ pages/skill.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
-import "../styles/global.scss";
 
-export default function Home() {
+const Skill = () => {
   return (
     <div className="container">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h2>Skill page</h2>
     </div>
   );
-}
+};
+export default Skill;
