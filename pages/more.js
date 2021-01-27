1c1af05bfb5501ec84dd85f5d8bb7769e9fb23e5
sancyo <b1845958@planet.kanazawa-it.ac.jp>
2021-01-27 15:38:15 +0900

    update:レイアウトを調整

--- pages/more.js
+++ pages/more.js
@@ -7,7 +7,7 @@ const More = ({ skill }) => {
   const skillArray = skill.slice();
   const normalSkillArray = skillArray.filter((item) => item.type === "中級");
   const easySkillArray = skillArray.filter((item) => item.type === "初級");
-  console.log(normalSkillArray);
+  const schoolSkillArray = skillArray.filter((item) => item.type === "学校");
   return (
     <div className="container">
       <Head>
@@ -18,30 +18,28 @@ const More = ({ skill }) => {
       <main className="container main-container">
         <div className="inner main-inner">
           <h2 className="main-title">スキル</h2>
-          <div className="skill-card-container">
-            <div className="skill-card-inner">
-              <h3 className="skill-type">開発で使用</h3>
-              <div className="skill-card-list">
-                {normalSkillArray.map((item) => {
-                  return <SkillCard value={item} key={item.id} />;
-                })}
-              </div>
+          <div className="skill-card-inner">
+            <h3 className="skill-type">開発で使用</h3>
+            <div className="skill-card-list">
+              {normalSkillArray.map((item) => {
+                return <SkillCard value={item} key={item.id} />;
+              })}
             </div>
-            <div className="skill-card-inner">
-              <h3 className="skill-type">勉強中</h3>
-              <div className="skill-card-list">
-                {easySkillArray.map((item) => {
-                  return <SkillCard value={item} key={item.id} />;
-                })}
-              </div>
+          </div>
+          <div className="skill-card-inner">
+            <h3 className="skill-type">勉強中</h3>
+            <div className="skill-card-list">
+              {easySkillArray.map((item) => {
+                return <SkillCard value={item} key={item.id} />;
+              })}
             </div>
-            <div className="skill-card-inner">
-              <h3 className="skill-type">大学で使用</h3>
-              <div className="skill-card-list">
-                {easySkillArray.map((item) => {
-                  return <SkillCard value={item} key={item.id} />;
-                })}
-              </div>
+          </div>
+          <div className="skill-card-inner">
+            <h3 className="skill-type">大学で使用</h3>
+            <div className="skill-card-list">
+              {schoolSkillArray.map((item) => {
+                return <SkillCard value={item} key={item.id} />;
+              })}
             </div>
           </div>
         </div>
@@ -55,7 +53,7 @@ export const getStaticProps = async () => {
     headers: { "X-API-KEY": process.env.API_KEY },
   };
   const skill = await fetch(
-    "https://designdock02.microcms.io/api/v1/skill",
+    "https://designdock02.microcms.io/api/v1/skill?limit=100",
     key
   )
     .then((res) => res.json())
